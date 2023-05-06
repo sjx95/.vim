68af72b5e229ce1ca56571a6bdfe63d7161ce0b9
FukoMaster <sjx95@outlook.com>
2023-05-06 19:15:31 +0800

    stash

--- init.vim
+++ init.vim
@@ -1,39 +1,12 @@
-call plug#begin()
-  Plug 'neoclide/coc.nvim', {'branch': 'release'}
-  Plug 'fatih/vim-go'
-  Plug 'sebdah/vim-delve'
-  Plug 'preservim/nerdtree'
-  Plug 'tpope/vim-fugitive'
-  Plug 'vim-airline/vim-airline'
-  Plug 'vim-airline/vim-airline-themes'
-  Plug 'preservim/tagbar'
-  Plug 'preservim/vimux'
-  Plug 'kien/ctrlp.vim'
-  Plug 'junegunn/fzf'
-  Plug 'junegunn/fzf.vim'
-  Plug 'dracula/vim', { 'as': 'dracula' }
-  Plug 'tomasr/molokai'
-call plug#end()
-
 " color scheme, true color
 set termguicolors
-colorscheme dracula
+"colorscheme dracula
 
 " disable vim-go :GoDef short cut (gd)
 " this is handled by LanguageClient [LC]
-let g:go_def_mapping_enabled = 0
-let g:go_gopls_enabled = 0
 autocmd FileType go let b:go_fmt_options = {'goimports': '-local ' . trim(system('cd '. shellescape(expand('%:p:h')) .' && go list -m'))}
 
-let g:coc_global_extensions = []
-let g:coc_global_extensions += ['coc-go', 'coc-clangd', 'coc-tsserver', 'coc-sh', 'coc-vimlsp']
-let g:coc_global_extensions += ['coc-protobuf', 'coc-thrift-syntax-support']
-let g:coc_global_extensions += ['coc-json', 'coc-yaml', 'coc-html']
-let g:coc_global_extensions += ['coc-cmake']
-let g:coc_global_extensions += ['coc-lists', 'coc-marketplace', 'coc-fzf-preview', 'coc-snippets']
 
-source ~/.config/nvim/config/vimrc.coc.default
-source ~/.config/nvim/config/vimrc.fatih.default
 
 " other settings
 set mouse=a
@@ -44,16 +17,12 @@ set wildmode=list:longest
 " tab settings
 set tabstop=4 shiftwidth=4
 autocmd FileType c,cpp setlocal et sta sw=2 sts=2
-autocmd FileType javascript setlocal et sta sw=2 sts=2
-autocmd FileType typescript setlocal et sta sw=2 sts=2
-autocmd FileType javascriptreact setlocal et sta sw=2 sts=2
-autocmd FileType typescriptreact setlocal et sta sw=2 sts=2
+autocmd FileType javascript,typescript,javascriptreact,typescriptreact
+			\setlocal et sta sw=2 sts=2
 
 " fold settings
 set foldmethod=syntax
 set foldlevelstart=99
 
-" shortcuts
-nmap tb :TagbarToggle<CR>
-nmap nt :NERDTreeToggle<CR>
-nmap nf :NERDTreeFind<CR>
+luafile ~/.config/nvim/xxx.lua
+
---
+++ xxx.lua
@@ -0,0 +1,158 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require"lazy".setup({
+	'neovim/nvim-lspconfig',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-cmdline',
+	'hrsh7th/nvim-cmp',
+	'saadparwaiz1/cmp_luasnip',
+	'L3MON4D3/LuaSnip',
+
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+
+ 
+	{'preservim/nerdtree', lazy = false, keys = {
+		{"nt", "<CMD>NERDTreeToggle<CR>", desc = "NERDTreeToggle" },
+		{"nf", "<CMD>NERDTreeFind<CR>", desc = "NERDTreeFind" },
+	}},
+
+	{'preservim/tagbar', lazy = false, keys = {
+		{"tb", "<CMD>TagbarToggle<CR>", desc = "TagbarToggle" },
+	}},
+	'vim-airline/vim-airline',
+	'vim-airline/vim-airline-themes',
+	{'tpope/vim-fugitive', lazy = false },
+	{'kien/ctrlp.vim', lazy = false },
+
+  'junegunn/fzf',
+  'junegunn/fzf.vim',
+   {
+    "dracula/vim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function() vim.cmd([[colorscheme dracula]])    end,
+  },
+  'tomasr/molokai',
+
+})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+--local use = require('packer').use
+--require('packer').startup(function()
+  --use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  --use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  --use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  --use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  --use 'L3MON4D3/LuaSnip' -- Snippets plugin
+--end)
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'clangd', 'gopls' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+  preselect = cmp.PreselectMode.None,
+}
+
+-- require'nvim-treesitter.configs'.setup {}
