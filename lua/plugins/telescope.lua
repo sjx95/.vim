f2b4664ffb07d44a68998101e1e8e43bec92e395
FukoMaster <sjx95@outlook.com>
2023-05-25 15:50:33 +0800

    Fix require telescope before installed by lazy.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,23 @@
+local builtin = setmetatable({}, {
+  __index =
+      function(_, key)
+        return function(...)
+          return require("telescope.builtin")[key](...)
+        end
+      end,
+})
+
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	lazy = false,
-	keys = function()
-		local builtin = require("telescope.builtin")
-		return {
-			{ "gd", builtin.lsp_definitions },
-			{ "gy", builtin.lsp_type_definitions },
-			{ "gr", builtin.lsp_references },
-			{ "gi", builtin.lsp_implementations },
-		}
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  keys = {
+    { "gd", builtin.lsp_definitions },
+    { "gy", builtin.lsp_type_definitions },
+    { "gr", builtin.lsp_references },
+    { "gi", builtin.lsp_implementations },
+  }
 }
