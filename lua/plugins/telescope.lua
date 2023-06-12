1f8a3d7fd9165ea51e066bccdbb4822e987fd274
FukoMaster <sjx95@outlook.com>
2023-06-12 11:16:14 +0800

    More telescope.nvim settings and basic dap (#4)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,25 @@
+local opts = {
+  defaults = {
+    layout_strategy = 'vertical',
+    layout_config = { preview_cutoff = 20 },
+  },
+  pickers = {
+    lsp_references = { fname_width = 0.5 },
+  },
+}
+
+local keys = {
+  { '<C-p>', '<CMD>Telescope find_files<CR>' },
+  { "gd",    '<CMD>Telescope lsp_definitions<CR>' },
+  { "gy",    '<CMD>Telescope lsp_type_definitions<CR>' },
+  { "gr",    '<CMD>Telescope lsp_references<CR>' },
+  { "gi",    '<CMD>Telescope lsp_implementations<CR>' },
+}
+
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	lazy = false,
-	keys = function()
-		local builtin = require("telescope.builtin")
-		return {
-			{ "gd", builtin.lsp_definitions },
-			{ "gy", builtin.lsp_type_definitions },
-			{ "gr", builtin.lsp_references },
-			{ "gi", builtin.lsp_implementations },
-		}
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", },
+  lazy         = false,
+  keys         = keys,
+  opts         = opts
 }
