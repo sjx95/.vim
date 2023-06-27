4da340961e06db618fdc0fd032c3032991da24d2
FukoMaster <sjx95@outlook.com>
2023-06-27 17:33:00 +0800

    Update protobuf settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,39 @@
 -- Get available servers at:
 -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 local mason_list = {
-	"vimls",
-	"lua_ls",
-	"bashls",
-	"jsonls",
-	"clangd",
-	"gopls",
-	"golangci_lint_ls",
-	"tsserver",
-	"eslint",
+  "vimls",
+  "lua_ls",
+  "bashls",
+  "jsonls",
+  "clangd",
+  "gopls",
+  "golangci_lint_ls",
+  "tsserver",
+  "eslint",
+  "bufls",
 }
 
+local language_server_options = {
+  clangd = { filetypes = { "c", "cpp", "objc", "objcpp", "cuda" } }
+}
+
+
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = { ensure_installed = mason_list, },
-		config = function(_, opts)
-			require("mason-lspconfig").setup(opts)
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					local capabilities = require('cmp_nvim_lsp').default_capabilities()
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
-		end,
-	},
-	{ "neovim/nvim-lspconfig" },
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig"
+  },
+  opts = {
+    ensure_installed = mason_list,
+    handlers = {
+      function(server_name)
+        local language_server_option = language_server_options[server_name]
+        if language_server_option == nil then
+          language_server_option = {}
+        end
+        require("lspconfig")[server_name].setup(language_server_option)
+      end,
+    }
+  },
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,20 +1,26 @@
 local mason_list = {
-	"goimports-reviser",
-	"gomodifytags",
+  "goimports-reviser",
+  "gomodifytags",
+  "clang_format",
 }
 
 return {
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			ensure_installed = mason_list,
-			handlers = {}, -- allow register all installed module automatically
-		},
-	},
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "jay-babu/mason-null-ls.nvim" },
-		opts = { sources = {} },
-	},
+  {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = mason_list,
+      handlers = {
+        ["clang_format"] = function()
+          local null_ls = require("null-ls")
+          null_ls.register(null_ls.builtins.formatting.clang_format.with({ filetypes = { "proto" }, }))
+        end,
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "jay-babu/mason-null-ls.nvim" },
+    opts = { sources = {} },
+  },
 }
