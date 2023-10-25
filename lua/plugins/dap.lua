69f021d15cde48402fd494cdb4beb0dc4a184555
FukoMaster <sjx95@outlook.com>
2023-10-25 19:06:43 +0800

    Add/update terminal, DAP and tree-sitter settings (#6)

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,9 +5,47 @@ return {
     lazy = false,
     opts = {
       ensure_installed = { 'delve' },
-      handlers = {}, -- sets up dap in the predefined manner
+      -- handlers = {}, -- sets up dap in the predefined manner
     },
   },
-  { "rcarriga/nvim-dap-ui", opts = {}, },
-  { 'leoluz/nvim-dap-go',   opts = {}, },
+  {
+    'leoluz/nvim-dap-go',
+    opts = {
+      -- delve configurations
+      delve = {
+        -- the path to the executable dlv which will be used for debugging.
+        -- by default, this is the "dlv" executable on your PATH.
+        path = "dlv",
+        -- time to wait for delve to initialize the debug session.
+        -- default to 20 seconds
+        initialize_timeout_sec = 20,
+        -- a string that defines the port to start delve debugger.
+        -- default to string "${port}" which instructs nvim-dap
+        -- to start the process in a random available port
+        port = "${port}",
+        -- additional args to pass to dlv
+        args = {},
+        -- the build flags that are passed to delve.
+        -- defaults to empty string, but can be used to provide flags
+        -- such as "-tags=unit" to make sure the test suite is
+        -- compiled during debugging, for example.
+        -- passing build flags using args is ineffective, as those are
+        -- ignored by delve in dap mode.
+        build_flags = "-gcflags=all=-l",
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = function()
+      local dapui = require("dapui")
+      dapui.setup()
+
+      local dap = require("dap")
+      dap.listeners.after.event_initialized["dapui"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui"] = function() dapui.close() end
+    end,
+  },
 }
