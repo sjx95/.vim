56714fdf56f22a88cb0832a1cf4710802d20766a
FukoMaster <sjx95@outlook.com>
2024-08-26 16:34:16 +0800

    Add nvim-treesitter-context and lazygit (#8)

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,8 +1,18 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  config = true,
-  keys = {
-    { "<leader>tt",    '<CMD>ToggleTerm<CR>' },
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true,
+    keys = {
+      { "<leader>tt", '<CMD>ToggleTerm<CR>' },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,4 +12,5 @@ return {
       indent = { enable = true },
     },
   },
+  { "nvim-treesitter/nvim-treesitter-context", opts = {} },
 }
