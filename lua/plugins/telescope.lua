c9b78922d18e53cac3100b7fa2dc187d2f22adfd
FukoMaster <sjx95@outlook.com>
2024-08-26 16:13:51 +0800

    Format telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,11 @@
+local keys = {
+  { '<C-p>', '<CMD>Telescope find_files<CR>' },
+  { "gd",    '<CMD>Telescope lsp_definitions<CR>' },
+  { "gy",    '<CMD>Telescope lsp_type_definitions<CR>' },
+  { "gr",    '<CMD>Telescope lsp_references<CR>' },
+  { "gi",    '<CMD>Telescope lsp_implementations<CR>' },
+}
+
 local opts = {
   defaults = {
     layout_strategy = 'vertical',
@@ -8,18 +16,16 @@ local opts = {
   },
 }
 
-local keys = {
-  { '<C-p>', '<CMD>Telescope find_files<CR>' },
-  { "gd",    '<CMD>Telescope lsp_definitions<CR>' },
-  { "gy",    '<CMD>Telescope lsp_type_definitions<CR>' },
-  { "gr",    '<CMD>Telescope lsp_references<CR>' },
-  { "gi",    '<CMD>Telescope lsp_implementations<CR>' },
-}
-
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    "sharkdp/fd",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
   lazy         = false,
   keys         = keys,
-  opts         = opts
+  opts         = opts,
 }
