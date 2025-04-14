af1b80ecd69d418f1073ec9ff601e7ba209e5693
FukoMaster <sjx95@outlook.com>
2025-04-14 16:33:08 +0800

    Modify keymaps

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -8,7 +8,7 @@ local keymaps = {
   { "<leader>ts", function() require("neotest").summary.toggle() end,                                 desc = "[t]est [s]ummary" },
   { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "[t]est [o]utput" },
   { "<leader>tO", function() require("neotest").output_panel.toggle() end,                            desc = "[t]est [O]utput panel" },
-  { "<leader>tt", function() require("neotest").run.stop() end,                                       desc = "[t]est [t]erminate" },
+  { "<leader>tT", function() require("neotest").run.stop() end,                                       desc = "[t]est [t]erminate" },
   { "<leader>td", function() require("neotest").run.run({ suite = false, strategy = "dap" }) end,     desc = "Debug nearest test" },
 }
 
